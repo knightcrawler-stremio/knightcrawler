457df6b3269eeb231012ed09650d2cc3e52152b6
iPromKnight <iPromKnight@StarCity.dc>
2024-02-06 10:47:59 +0000

    remove boolean helpers class

--- src/node/consumer/src/lib/helpers/boolean_helpers.ts
+++ src/node/consumer/src/lib/helpers/boolean_helpers.ts
@@ -1,5 +1,5 @@
-export class BooleanHelpers {
-    public static parseBool(value: string, defaultValue: boolean): boolean {
+export const BooleanHelpers = {
+    parseBool: function(value: string | undefined, defaultValue: boolean) {
         if (value === undefined) {
             return defaultValue;
         }
