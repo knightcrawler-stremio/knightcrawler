c61e9e94e1d42249996c64b234cb31e005bc15ca
iPromKnight <iPromKnight@StarCity.dc>
2024-03-03 19:22:10 +0000

    Rethrow so polly captures failures on requests.

--- src/producer/Features/Crawlers/Torrentio/TorrentioInstanceExtensions.cs
+++ src/producer/Features/Crawlers/Torrentio/TorrentioInstanceExtensions.cs
@@ -21,20 +21,7 @@ public static class TorrentioInstancesExtensions
 
         return remaining > TimeSpan.Zero ? remaining : TimeSpan.Zero;
     }
-    
-    public static void SetPossiblyRateLimited(this TorrentioInstance instance, Dictionary<string, TorrentioScrapeInstance> scraperState, int minutesToWait = 5)
-    {
-        var state = instance.EnsureStateExists(scraperState);
 
-        // Set the start time to 15 minutes in the past so that the next check will result in a rate limit period of 15 minutes
-        var startedAt = DateTime.UtcNow.AddMinutes(-minutesToWait);
-        var requestCount = instance.RateLimit.RequestLimit;
-
-        // Update the scraper state for the instance
-        state.StartedAt = startedAt;
-        state.RequestCount = requestCount;
-    }
-    
     public static long TotalProcessedRequests(this TorrentioInstance instance, Dictionary<string, TorrentioScrapeInstance> scraperState) => 
         !scraperState.TryGetValue(instance.Name, out var state) ? 0 : state.TotalProcessed;
     
