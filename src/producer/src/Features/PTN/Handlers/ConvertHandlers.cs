11552ab64c708e7a1b63831840cfbecf88300dde
iPromKnight <promknight@proton.me>
2024-03-13 23:28:53 +0000

    [skip ci] Convert handlers tests

--- src/producer/src/Features/PTN/Handlers/ConvertHandlers.cs
+++ src/producer/src/Features/PTN/Handlers/ConvertHandlers.cs
@@ -2,5 +2,5 @@ namespace Producer.Features.PTN.Handlers;
 
 public class ConvertHandlers : IPtnHandler
 {
-    public void RegisterHandlers(IParseTorrentName parser) => parser.AddHandler(ResultKeys.Convert, new Regex(@"CONVERT"), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
+    public void RegisterHandlers(IParseTorrentName parser) => parser.AddHandler(ResultKeys.Convert, new Regex(@"(CONVERT)"), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
 }
