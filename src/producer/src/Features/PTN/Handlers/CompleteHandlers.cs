e7ced2a678ced9973bb4febe15f6dc59d36e8e26
iPromKnight <promknight@proton.me>
2024-03-13 23:25:27 +0000

    [skip ci] Complete Collection handler tests

--- src/producer/src/Features/PTN/Handlers/CompleteHandlers.cs
+++ src/producer/src/Features/PTN/Handlers/CompleteHandlers.cs
@@ -6,11 +6,11 @@ public class CompleteHandlers : IPtnHandler
     {
         parser.AddHandler(ResultKeys.Complete, new Regex(@"(?:\bthe\W)?(?:\bcomplete|collection|dvd)?\b[ .]?\bbox[ .-]?set\b", RegexOptions.IgnoreCase), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
         parser.AddHandler(ResultKeys.Complete, new Regex(@"(?:\bthe\W)?(?:\bcomplete|collection|dvd)?\b[ .]?\bmini[ .-]?series\b", RegexOptions.IgnoreCase), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
-        parser.AddHandler(ResultKeys.Complete, new Regex(@"(?:\bthe\W)?(?:\bcomplete|full|all)\b.*\b(?:series|seasons|collection|episodes|set|pack|movies)\b", RegexOptions.IgnoreCase), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
-        parser.AddHandler(ResultKeys.Complete, new Regex(@"\b(?:series|seasons|movies?)\b.*\b(?:complete|collection)\b", RegexOptions.IgnoreCase), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
+        parser.AddHandler(ResultKeys.Complete, new Regex(@"(?:\bthe\W)?(?:(\bcomplete|full|all)\b).*\b(?:series|seasons|collection|episodes|set|pack|movies)\b", RegexOptions.IgnoreCase), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
+        parser.AddHandler(ResultKeys.Complete, new Regex(@"(\b(?:series|seasons|movies?)\b.*\b(?:complete|collection)\b)", RegexOptions.IgnoreCase), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
         parser.AddHandler(ResultKeys.Complete, new Regex(@"(?:\bthe\W)?\bultimate\b[ .]\bcollection\b", RegexOptions.IgnoreCase), Transformers.Boolean, new() { SkipIfAlreadyFound = false });
-        parser.AddHandler(ResultKeys.Complete, new Regex(@"\bcollection\b.*\b(?:set|pack|movies)\b", RegexOptions.IgnoreCase), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
-        parser.AddHandler(ResultKeys.Complete, new Regex(@"\bcollection\b", RegexOptions.IgnoreCase), Transformers.Boolean, new() { SkipFromTitle = true });
-        parser.AddHandler(ResultKeys.Complete, new Regex(@"duology|trilogy|quadr[oi]logy|tetralogy|pentalogy|hexalogy|heptalogy|anthology|saga", RegexOptions.IgnoreCase), Transformers.Boolean, new() { SkipIfAlreadyFound = false });
+        parser.AddHandler(ResultKeys.Complete, new Regex(@"(\bcollection\b.*\b(?:set|pack|movies)\b)", RegexOptions.IgnoreCase), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
+        parser.AddHandler(ResultKeys.Complete, new Regex(@"(\bcollection\b)", RegexOptions.IgnoreCase), Transformers.Boolean, new() { SkipFromTitle = false });
+        parser.AddHandler(ResultKeys.Complete, new Regex(@"(duology|trilogy|quadr[oi]logy|tetralogy|pentalogy|hexalogy|heptalogy|anthology|saga)", RegexOptions.IgnoreCase), Transformers.Boolean, new() { SkipIfAlreadyFound = false });
     }
 }
