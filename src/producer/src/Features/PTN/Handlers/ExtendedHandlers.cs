e69b934445c2c372051aa5d77adc89ea81a852a8
iPromKnight <promknight@proton.me>
2024-03-14 00:37:44 +0000

    [skip ci] Extended handler tests

--- src/producer/src/Features/PTN/Handlers/ExtendedHandlers.cs
+++ src/producer/src/Features/PTN/Handlers/ExtendedHandlers.cs
@@ -2,5 +2,5 @@ namespace Producer.Features.PTN.Handlers;
 
 public class ExtendedHandlers : IPtnHandler
 {
-    public void RegisterHandlers(IParseTorrentName parser) => parser.AddHandler(ResultKeys.Extended, new Regex(@"EXTENDED"), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
+    public void RegisterHandlers(IParseTorrentName parser) => parser.AddHandler(ResultKeys.Extended, new Regex(@"(EXTENDED)"), Transformers.Boolean, ParseTorrentNameOptions.DefaultOptions);
 }
