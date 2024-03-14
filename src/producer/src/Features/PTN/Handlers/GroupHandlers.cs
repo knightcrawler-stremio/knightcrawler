1bfa2a6867216a9f662f8e92ec3e3cd372335106
iPromKnight <promknight@proton.me>
2024-03-14 00:48:34 +0000

    [skip ci] group handler tests

--- src/producer/src/Features/PTN/Handlers/GroupHandlers.cs
+++ src/producer/src/Features/PTN/Handlers/GroupHandlers.cs
@@ -5,7 +5,7 @@ public class GroupHandlers : IPtnHandler
     public void RegisterHandlers(IParseTorrentName parser)
     {
         parser.AddHandler(ResultKeys.Group, new Regex(@"- ?(?!\d+$|S\d+|\d+x|ep?\d+|[^[]+]$)([^\-. []+[^\-. [)\]\d][^\-. [)\]]*)(?:\[[\w.-]+])?(?=\.\w{2,4}$|$)", RegexOptions.IgnoreCase), Transformers.None, new() {Remove = true});
-        parser.AddHandler(ResultKeys.Group, new Regex(@"^\[([^[\]]+)]", RegexOptions.IgnoreCase), Transformers.None, ParseTorrentNameOptions.DefaultOptions);
+        parser.AddHandler(ResultKeys.Group, new Regex(@"^\[((?!DVD)[^\[\]]+)]", RegexOptions.IgnoreCase), Transformers.None, ParseTorrentNameOptions.DefaultOptions);
         parser.AddHandler(ResultKeys.Group, new Regex(@"\(([\w-]+)\)(?:$|\.\w{2,4}$)", RegexOptions.IgnoreCase), Transformers.None, ParseTorrentNameOptions.DefaultOptions);
         parser.AddHandler(ResultKeys.Group, Handler.CreateHandlerFromDelegate(ResultKeys.Group, AnimeMatch), Transformers.None, ParseTorrentNameOptions.DefaultOptions);
     }
