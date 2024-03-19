b74b118293b184a4c34d2ef9d89940949b59ef93
Brett <160749165+RohirrimRider@users.noreply.github.com>
2024-03-19 00:16:19 -0500

    works

--- annatar/torrent_ingestor/torrent_ingestor/main.py
+++ annatar/torrent_ingestor/torrent_ingestor/main.py
@@ -1,4 +1,6 @@
 import asyncio
+from sqlalchemy.exc import IntegrityError
+import contextlib
 from os import environ
 from datetime import datetime
 from redis import asyncio as redis
@@ -6,6 +8,7 @@ from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+from concurrent.futures import ThreadPoolExecutor
 
 # Define SQLAlchemy model (for DB interaction)
 Base = declarative_base()
@@ -16,6 +19,7 @@ class TorrentAddedEvent(BaseModel):
     info_hash: str
     title: str
     imdb: str
+    size: int
     indexer: str
     category: str
     season: int | None = None
@@ -43,17 +47,28 @@ async def write_to_postgres(
     queue: asyncio.Queue[TorrentAddedEvent],
 ) -> None:
     while True:
-        event: TorrentAddedEvent = await queue.get()
-        session().add(
-            IngestedTorrent(
-                name=event.title,
-                info_hash=event.info_hash,
-                imdb=event.imdb,
-                category=event.category,
-                updatedAt=datetime.utcnow(),
-            )
-        )
-        queue.task_done()
+        sess = session()
+        try:
+            event: TorrentAddedEvent = await queue.get()
+            with contextlib.suppress(IntegrityError):
+                sess.add(
+                    IngestedTorrent(
+                        name=event.title,
+                        info_hash=event.info_hash,
+                        imdb=event.imdb,
+                        size=event.size,
+                        category=event.category,
+                        updatedAt=datetime.utcnow(),
+                    )
+                )
+                sess.commit()
+                print("saved torrent", event.info_hash)
+            queue.task_done()
+        except Exception as e:
+            print("error saving torrent", e)
+            await asyncio.sleep(1)
+        finally:
+            sess.close()
 
 
 async def redis_subscriber(
@@ -64,12 +79,15 @@ async def redis_subscriber(
     await pubsub.subscribe(channel_name)
     try:
         async for message in pubsub.listen():
+            print("got message:", message)
             if message["type"] == "message":
-                event_data = TorrentAddedEvent.model_validate(message["data"])
+                event_data = TorrentAddedEvent.model_validate_json(
+                    message["data"].decode()
+                )
                 await queue.put(event_data)
     finally:
         await pubsub.unsubscribe(channel_name)
-        await db.close()
+        await db.aclose()
 
 
 async def main():
@@ -82,7 +100,7 @@ async def main():
     if not DATABASE_URL:
         raise ValueError("POSTGRES_URL env var is required.")
     engine = create_engine(
-        DATABASE_URL, pool_size=10, max_overflow=0, pool_timeout=5, pool_recycle=3600
+        DATABASE_URL, pool_size=100, max_overflow=0, pool_timeout=5, pool_recycle=3600
     )
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -95,11 +113,23 @@ async def main():
     subscriber_task = asyncio.create_task(
         redis_subscriber(REDIS_URL, "events:v1:torrent:added", queue)
     )
-    writer_task = asyncio.create_task(write_to_postgres(SessionLocal, queue))
+
+    writer_task = asyncio.create_task(run_queue_processors(SessionLocal, queue, 10))
 
     await asyncio.wait(
         [subscriber_task, writer_task], return_when=asyncio.FIRST_COMPLETED
     )
+
+
+async def run_queue_processors(
+    session: sessionmaker[Session], queue: asyncio.Queue, n_processors: int
+):
+    with ThreadPoolExecutor(max_workers=n_processors) as executor:
+        tasks = [
+            asyncio.create_task(write_to_postgres(session, queue))
+            for _ in range(n_processors)
+        ]
+        await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
