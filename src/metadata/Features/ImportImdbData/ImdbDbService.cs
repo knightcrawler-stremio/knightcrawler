99f8ceae058bfa0cc1b5a3c2a32854efba530f2a
iPromKnight <promknight@proton.me>
2024-03-20 15:11:36 +0000

    Cleaner rerunning of metadata - without conflicts

--- src/metadata/Features/ImportImdbData/ImdbDbService.cs
+++ src/metadata/Features/ImportImdbData/ImdbDbService.cs
@@ -2,48 +2,64 @@ namespace Metadata.Features.ImportImdbData;
 
 public class ImdbDbService(PostgresConfiguration configuration, ILogger<ImdbDbService> logger)
 {
-    public async Task InsertImdbEntries(IEnumerable<ImdbEntry> entries)
-    {
-        const string sqlCommand = "COPY imdb_metadata (\"imdb_id\", \"category\", \"title\", \"year\", \"adult\") FROM STDIN (FORMAT BINARY)";
-        
-        try
-        {
-            await using var connection = new NpgsqlConnection(configuration.StorageConnectionString);
-            await connection.OpenAsync();
-
-            await using var writer = await connection.BeginBinaryImportAsync(sqlCommand);
-
-            foreach (var entry in entries)
+    public Task InsertImdbEntries(IEnumerable<ImdbEntry> entries) =>
+        ExecuteCommandAsync(
+            async connection =>
             {
-                try
+                await using var writer = await connection.BeginBinaryImportAsync(
+                    "COPY imdb_metadata (\"imdb_id\", \"category\", \"title\", \"year\", \"adult\") FROM STDIN (FORMAT BINARY)");
+
+                foreach (var entry in entries)
                 {
-                    await writer.StartRowAsync();
-                    await writer.WriteAsync(entry.ImdbId, NpgsqlDbType.Text);
-                    await writer.WriteAsync(entry.Category, NpgsqlDbType.Text);
-                    await writer.WriteAsync(entry.Title, NpgsqlDbType.Text);
-                    await writer.WriteAsync(entry.Year, NpgsqlDbType.Text);
-                    await writer.WriteAsync(entry.Adult, NpgsqlDbType.Boolean);
-                }
-                catch (NpgsqlException e)
-                {
-                    if (e.Message.Contains("duplicate key value violates unique constraint", StringComparison.OrdinalIgnoreCase))
+                    try
                     {
-                        continue;
+                        await writer.StartRowAsync();
+                        await writer.WriteAsync(entry.ImdbId, NpgsqlDbType.Text);
+                        await writer.WriteAsync(entry.Category, NpgsqlDbType.Text);
+                        await writer.WriteAsync(entry.Title, NpgsqlDbType.Text);
+                        await writer.WriteAsync(entry.Year, NpgsqlDbType.Text);
+                        await writer.WriteAsync(entry.Adult, NpgsqlDbType.Boolean);
                     }
-                    
-                    throw;
+                    catch (NpgsqlException e)
+                    {
+                        if (e.Message.Contains("duplicate key value violates unique constraint", StringComparison.OrdinalIgnoreCase))
+                        {
+                            continue;
+                        }
+
+                        throw;
+                    }
                 }
-            }
 
-            await writer.CompleteAsync();
-        }
-        catch (Exception e)
-        {
-            logger.LogError(e, "Error while inserting imdb entries into database");
-        }
-    }
+                await writer.CompleteAsync();
+            }, "Error while inserting imdb entries into database");
+    
+    public Task TruncateTable() =>
+        ExecuteCommandAsync(
+            async connection =>
+            {
+                await using var command = new NpgsqlCommand("TRUNCATE TABLE imdb_metadata", connection);
+                await command.ExecuteNonQueryAsync();
+            }, "Error while clearing 'imdb_metadata' table");
 
-    public async Task EnsureIndexExistsForNewData()
+    public Task CreatePgtrmIndex() =>
+        ExecuteCommandAsync(
+            async connection =>
+            {
+                await using var command = new NpgsqlCommand("CREATE INDEX title_gist ON imdb_metadata USING gist(title gist_trgm_ops)", connection);
+                await command.ExecuteNonQueryAsync();
+            }, "Error while creating index on imdb_metadata table");
+
+    public Task DropPgtrmIndex() =>
+        ExecuteCommandAsync(
+            async connection =>
+            {
+                await using var dropCommand = new NpgsqlCommand("DROP INDEX if exists title_gist", connection);
+                await dropCommand.ExecuteNonQueryAsync();
+            }, "Error while dropping index on imdb_metadata table");
+
+
+    private async Task ExecuteCommandAsync(Func<NpgsqlConnection, Task> operation, string errorMessage)
     {
         try
         {
@@ -55,50 +71,11 @@ public class ImdbDbService(PostgresConfiguration configuration, ILogger<ImdbDbSe
             await using var connection = new NpgsqlConnection(connectionStringBuilder.ConnectionString);
             await connection.OpenAsync();
 
-            await DropPgtrmIndex(connection);
-            await CreatePgtrmIndex(connection);
+            await operation(connection);
         }
         catch (Exception e)
         {
-            logger.LogError(e, "Error while creating index on imdb_metadata table");
-        }
-    }
-    
-    private async Task DropPgtrmIndex(NpgsqlConnection connection)
-    {
-        const string checkIndexExistsCommand = "SELECT 1 FROM pg_indexes WHERE indexname = 'title_gist'";
-
-        await using var checkCommand = new NpgsqlCommand(checkIndexExistsCommand, connection);
-        var indexExists = await checkCommand.ExecuteScalarAsync();
-
-        if (indexExists != null)
-        {
-            const string dropIndexCommand = "DROP INDEX title_gist";
-
-            try
-            {
-                await using var dropCommand = new NpgsqlCommand(dropIndexCommand, connection);
-                await dropCommand.ExecuteNonQueryAsync();
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, "Error while dropping index on imdb_metadata table");
-            }
-        }
-    }
-
-    private async Task CreatePgtrmIndex(NpgsqlConnection connection)
-    {
-        const string sqlCommand = "CREATE INDEX title_gist ON imdb_metadata USING gist(title gist_trgm_ops)";
-        
-        try
-        {
-            await using var command = new NpgsqlCommand(sqlCommand, connection);
-            await command.ExecuteNonQueryAsync();
-        }
-        catch (Exception e)
-        {
-            logger.LogError(e, "Error while creating index on imdb_metadata table");
+            logger.LogError(e, errorMessage);
         }
     }
 }
