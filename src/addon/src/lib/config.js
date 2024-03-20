1a22f8de5653851197366cafd1b28288bc7492ee
iPromKnight <promknight@proton.me>
2024-03-20 13:10:08 +0000

    [skip ci] change mongo to redis in the addon

--- src/addon/src/lib/config.js
+++ src/addon/src/lib/config.js
@@ -1,17 +1,7 @@
 export const cacheConfig = {
-    MONGODB_HOST: process.env.MONGODB_HOST || 'mongodb',
-    MONGODB_PORT: process.env.MONGODB_PORT || '27017',
-    MONGODB_DB: process.env.MONGODB_DB || 'knightcrawler',
-    MONGODB_USER: process.env.MONGODB_USER || 'mongo',
-    MONGODB_PASSWORD: process.env.MONGODB_PASSWORD || 'mongo',
-    COLLECTION_NAME: process.env.MONGODB_ADDON_COLLECTION || 'knightcrawler_addon_collection',
+    REDIS_CONNECTION_STRING: process.env.REDIS_CONNECTION_STRING || 'redis://localhost:6379/0',
     NO_CACHE: parseBool(process.env.NO_CACHE, false),
 }
-
-// Combine the environment variables into a connection string
-// The combined string will look something like:
-// 'mongodb://mongo:mongo@localhost:27017/knightcrawler?authSource=admin'
-cacheConfig.MONGO_URI = 'mongodb://' + cacheConfig.MONGODB_USER + ':' + cacheConfig.MONGODB_PASSWORD + '@' + cacheConfig.MONGODB_HOST + ':' + cacheConfig.MONGODB_PORT + '/' + cacheConfig.MONGODB_DB + '?authSource=admin';
 
 export const databaseConfig = {
     POSTGRES_HOST: process.env.POSTGRES_HOST || 'postgres',
