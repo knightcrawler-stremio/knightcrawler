136e3af52d2db2f4b03cf020d6c3bae54bc45c54
iPromKnight <promknight@proton.me>
2024-03-22 14:50:11 +0000

    reverse order to avoid cascadeing

--- src/metadata/Features/ImportImdbData/ImportImdbDataRequestHandler.cs
+++ src/metadata/Features/ImportImdbData/ImportImdbDataRequestHandler.cs
@@ -23,7 +23,9 @@ public class ImportImdbDataRequestHandler(
     {
         logger.LogInformation("Clearing existing IMDB data from database");
         await dbService.DropPgtrmIndex();
-        await dbService.TruncateTable(TableNames.MetadataTable);
+        await dbService.TruncateTable(TableNames.EpisodesTable);
+        await dbService.TruncateTable(TableNames.AkasTable);
+        await dbService.TruncateTable(TableNames.MetadataTable, cascade: true);
         logger.LogInformation("Existing IMDB data cleared from database");
     }
 }
