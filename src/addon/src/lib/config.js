c3e58e4234cfad91eaa9986b5422bdb4f4c16905
iPromKnight <156901906+iPromKnight@users.noreply.github.com>
2024-03-26 09:26:35 +0000

    Fix redis connection strings for consistency across languages. (#168)
    * Fix redis connection strings across languages
    
    * compose version bump

--- src/addon/src/lib/config.js
+++ src/addon/src/lib/config.js
@@ -1,7 +1,11 @@
 export const cacheConfig = {
-    REDIS_CONNECTION_STRING: process.env.REDIS_CONNECTION_STRING || 'redis://localhost:6379/0',
+    REDIS_HOST: process.env.REDIS_HOST || 'redis',
+    REDIS_PORT: process.env.REDIS_PORT || '6379',
+    REDIS_EXTRA: process.env.REDIS_EXTRA || '',
     NO_CACHE: parseBool(process.env.NO_CACHE, false),
 }
+
+cacheConfig.REDIS_CONNECTION_STRING = 'redis://' + cacheConfig.REDIS_HOST + ':' + cacheConfig.REDIS_PORT + cacheConfig.REDIS_EXTRA;
 
 export const databaseConfig = {
     POSTGRES_HOST: process.env.POSTGRES_HOST || 'postgres',
