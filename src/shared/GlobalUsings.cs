c493ef3376caeb6a17ce3a76a4b894c434cd8fe2
iPromKnight <156901906+iPromKnight@users.noreply.github.com>
2024-03-30 04:47:36 +0000

    Hotfix category, and roll back RTN to 0.1.8 (#192)
    * Hotfix categories
    
    Also roll back RTN to 0.1.8 as regression introduced in 0.2
    
    * bump version

--- src/shared/GlobalUsings.cs
+++ src/shared/GlobalUsings.cs
@@ -2,6 +2,7 @@
 
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
+global using System.Text.RegularExpressions;
 global using Dapper;
 global using MassTransit;
 global using Microsoft.AspNetCore.Builder;
