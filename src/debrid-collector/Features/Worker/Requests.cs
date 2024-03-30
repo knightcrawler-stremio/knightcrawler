f85179c91ae6f08c4aa50236d509ecd116c7b144
iPromKnight <promknight@proton.me>
2024-03-30 00:07:14 +0000

    ensure unique message contract names per collector type

--- src/debrid-collector/Features/Worker/Requests.cs
+++ src/debrid-collector/Features/Worker/Requests.cs
@@ -1,21 +1,21 @@
 ﻿namespace DebridCollector.Features.Worker;
 
-[EntityName("perform-metadata-request")]
+[EntityName("perform-metadata-request-debrid-collector")]
 public record PerformMetadataRequest(Guid CorrelationId, string InfoHash) : CorrelatedBy<Guid>;
 
-[EntityName("torrent-metadata-response")]
+[EntityName("torrent-metadata-response-debrid-collector")]
 public record GotMetadata(TorrentMetadataResponse Metadata) : CorrelatedBy<Guid>
 {
     public Guid CorrelationId { get; init; } = Metadata.CorrelationId;
 }
 
-[EntityName("write-metadata")]
+[EntityName("write-metadata-debrid-collector")]
 public record WriteMetadata(Torrent Torrent, TorrentMetadataResponse Metadata, string ImdbId) : CorrelatedBy<Guid>
 {
     public Guid CorrelationId { get; init; } = Metadata.CorrelationId;
 }
 
-[EntityName("metadata-written")]
+[EntityName("metadata-written-debrid-colloctor")]
 public record MetadataWritten(TorrentMetadataResponse Metadata) : CorrelatedBy<Guid>
 {
     public Guid CorrelationId { get; init; } = Metadata.CorrelationId;
--- src/qbit-collector/Features/Worker/Requests.cs
+++ src/qbit-collector/Features/Worker/Requests.cs
@@ -1,22 +1,24 @@
 ﻿namespace QBitCollector.Features.Worker;
 
-[EntityName("perform-metadata-request")]
+[EntityName("perform-metadata-request-qbit-collector")]
 public record PerformQbitMetadataRequest(Guid CorrelationId, string InfoHash) : CorrelatedBy<Guid>;
 
-[EntityName("torrent-metadata-response")]
+[EntityName("torrent-metadata-response-qbit-collector")]
 public record GotQbitMetadata(QBitMetadataResponse Metadata) : CorrelatedBy<Guid>
 {
     public Guid CorrelationId { get; init; } = Metadata.CorrelationId;
 }
 
-[EntityName("write-metadata")]
+[EntityName("write-metadata-qbit-collector")]
 public record WriteQbitMetadata(Torrent Torrent, QBitMetadataResponse Metadata, string ImdbId) : CorrelatedBy<Guid>
 {
     public Guid CorrelationId { get; init; } = Metadata.CorrelationId;
 }
 
-[EntityName("metadata-written")]
+[EntityName("metadata-written-qbit-collector")]
 public record QbitMetadataWritten(QBitMetadataResponse Metadata) : CorrelatedBy<Guid>
 {
     public Guid CorrelationId { get; init; } = Metadata.CorrelationId;
+
+    public QBitMetadataResponse Metadata { get; init; } = Metadata;
 }
