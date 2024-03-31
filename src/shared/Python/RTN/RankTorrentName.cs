967197c3dd838efa0cc3c820523b9db566632520
iPromKnight <promknight@proton.me>
2024-03-31 12:17:44 +0100

    Implement movie / show type parsing

--- src/shared/Python/RTN/RankTorrentName.cs
+++ src/shared/Python/RTN/RankTorrentName.cs
@@ -17,10 +17,10 @@ public class RankTorrentName : IRankTorrentName
         _pythonEngineService.ExecutePythonOperationWithDefault(
             () =>
             {
-                var result = _rtn?.parse(title);
+                var result = _rtn?.parse(title, true);
                 return ParseResult(result);
             }, new ParseTorrentTitleResponse(false, null), nameof(Parse), throwOnErrors: false, logErrors: false);
-    
+
     private static ParseTorrentTitleResponse ParseResult(dynamic result)
     {
         if (result == null)
@@ -34,9 +34,18 @@ public class RankTorrentName : IRankTorrentName
         {
             return new(false, null);
         }
+
+        var mediaType = result.GetAttr("type")?.As<string>();
+        
+        if (string.IsNullOrEmpty(mediaType))
+        {
+            return new(false, null);
+        }
         
         var response = JsonSerializer.Deserialize<RtnResponse>(json);
-
+        
+        response.IsMovie = mediaType.Equals("movie", StringComparison.OrdinalIgnoreCase);
+        
         return new(true, response);
     }
 
