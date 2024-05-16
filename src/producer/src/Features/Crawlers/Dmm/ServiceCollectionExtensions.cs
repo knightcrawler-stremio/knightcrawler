dded2d84ebf1618b2ae9b3603843a5934ccf8bc8
FunkeCoder23 <12570656+FunkeCoder23@users.noreply.github.com>
2024-05-15 20:59:42 -0400

    Add longer timeout for DMM

--- src/producer/src/Features/Crawlers/Dmm/ServiceCollectionExtensions.cs
+++ src/producer/src/Features/Crawlers/Dmm/ServiceCollectionExtensions.cs
@@ -9,6 +9,7 @@ public static class ServiceCollectionExtensions
             client.BaseAddress = new("https://github.com/debridmediamanager/hashlists/zipball/main/");
             client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip");
             client.DefaultRequestHeaders.UserAgent.ParseAdd("curl");
+            client.Timeout = TimeSpan.FromMinutes(10); // 10 minute timeout, #217
         });
 
         return services;
